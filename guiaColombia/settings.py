48122ede59d4e0d1fa192e7a1b31a668f52c7abf
Armando Acosta Guerrero <darman.89@gmail.com>
2019-08-16 16:46:45 -0500

    - Activando Django-Heroku tools - Implementando sslmode issue workaround

--- guiaColombia/settings.py
+++ guiaColombia/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'guiaColombia.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
+DATABASES = {'default': dj_database_url.config()}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
