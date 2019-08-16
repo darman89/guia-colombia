a961b37fba5a355d34f3cf5c6923f6cf436f1bf7
Armando Acosta Guerrero <darman.89@gmail.com>
2019-08-16 13:58:33 -0500

    - Configurando Base de datos a través de dj-config-url - Desactivando Django-Heroku tools

--- guiaColombia/settings.py
+++ guiaColombia/settings.py
@@ -129,6 +129,3 @@ STATICFILES_DIRS = (
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
