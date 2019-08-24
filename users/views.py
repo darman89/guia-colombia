5f234592eb8686c148138911ab5046717e7872b3
carlos <cdaza933@hotmail.com>
2019-08-24 12:03:50 -0500

    user serializer and default view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.mixins import CreateModelMixin
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from users.serializers import UserSerializer
+
+
+class UserViewSet(GenericViewSet, CreateModelMixin):
+    queryset = Costumer.objects.exclude(is_staff=False)
+    serializer_class = UserSerializer
