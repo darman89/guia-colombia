81985aafa6b7a10c688edbb3e149c5667336ba80
miarevalo10 <mariaisabelarevalo@hotmail.com>
2019-08-25 13:44:16 -0500

    ajuste usersviewset

--- users/views.py
+++ users/views.py
@@ -2,8 +2,10 @@ from rest_framework.mixins import CreateModelMixin
 from rest_framework.viewsets import GenericViewSet
 
 from users.serializers import UserSerializer
+from .models import User
 
 
 class UserViewSet(GenericViewSet, CreateModelMixin):
-    queryset = Costumer.objects.exclude(is_staff=False)
+    queryset = User.objects.exclude(is_staff=False)
     serializer_class = UserSerializer
+
