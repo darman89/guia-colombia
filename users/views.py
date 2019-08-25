8d39b54df9807e2c6f423ed7082c88eaefc702e0
carlos <cdaza933@hotmail.com>
2019-08-25 12:29:38 -0500

    add missing url

--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
 from rest_framework.mixins import CreateModelMixin
 from rest_framework.viewsets import GenericViewSet
 
+from users.models import User
 from users.serializers import UserSerializer
 
 
 class UserViewSet(GenericViewSet, CreateModelMixin):
-    queryset = Costumer.objects.exclude(is_staff=False)
+    queryset = User.objects.exclude(is_staff=False)
     serializer_class = UserSerializer
