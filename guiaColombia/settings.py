28c907239c3b7b72dad1820e89da3536c41a05a7
Maria Arevalo <mariaisabelarevalo@hotmail.com>
2019-08-25 16:58:58 -0500

    added cors middleware

--- guiaColombia/settings.py
+++ guiaColombia/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
