e37b001348b44e8a8e077b614fee87e08c572888
Catalina <cata.cordoba8@gmail.com>
2019-08-31 21:48:06 -0500

    Add serializer for updating user data and add auth token

--- guiaColombia/settings.py
+++ guiaColombia/settings.py
@@ -50,8 +50,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',)
 }
 
 AUTH_USER_MODEL = 'users.User'
--- users/views.py
+++ users/views.py
@@ -2,9 +2,11 @@ from rest_framework.mixins import CreateModelMixin, UpdateModelMixin, RetrieveMo
 from rest_framework.viewsets import GenericViewSet
 
 from users.models import User
+from users.permissions import IsTheUserOrCreate
 from users.serializers import UserSerializer
 
 
 class UserViewSet(GenericViewSet, CreateModelMixin, UpdateModelMixin, RetrieveModelMixin):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (IsTheUserOrCreate,)
